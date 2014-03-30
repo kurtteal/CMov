83993cb553e4b897216a0fc6bd5f57ae18590238
Pedro Madeira <pedro.martins.madeira@gmail.com>
2014-03-30 13:28:02 +0100

    Added game models(Wall, Path, Bomberman, Robot, Bomb).

--- BomberMan/src/com/example/bomberman/model/Wall.java
+++ BomberMan/src/com/example/bomberman/model/Wall.java
@@ -7,72 +7,64 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 
 import com.example.bomberman.R;
-import com.example.bomberman.model.components.Speed;
 
 /**
- * This is a test droid that is dragged, dropped, moved, smashed against
- * the wall and done other terrible things with.
- * Wait till it gets a weapon!
+ * This is a test droid that is dragged, dropped, moved, smashed against the
+ * wall and done other terrible things with. Wait till it gets a weapon!
  * 
  * @author impaler
- *
+ * 
  */
 public class Wall {
 
-	private Bitmap bitmapRight;	// the actual bitmap (or the animation sequence)
-	private Bitmap bitmapLeft;
-	private int x;			// the X coordinate (top left of the image)
-	private int y;			// the Y coordinate (top left of the image)
-	private boolean touched;	// if droid is touched/picked up
-	private Speed speed;	// the speed with its directions
-	
-	private static final String TAG = Wall.class.getSimpleName();
-	private Rect sourceRect; // the rectangle to be drawn from the animation bitmap
-	private int frameNr = 5; // number of frames in animation
-	private int currentFrame; // the current frame
-	private long frameTicker; // the time of the last frame update
+	private Bitmap bitmap; // the actual bitmap (or the animation sequence)
+	private int x; // the X coordinate (top left of the image)
+	private int y; // the Y coordinate (top left of the image)
 
-	private int fps = 5; //da animacao, n eh do jogo
-	private int framePeriod = 1000 / fps; // milliseconds between each frame (1000/fps)
-	private int spriteWidth; // the width of the sprite to calculate the cut out rectangle
-	private int spriteHeight;   // the height of the sprite
-	
-	public Wall (Resources resources, int x, int y) {
-		this.bitmapRight = BitmapFactory.decodeResource(resources, R.drawable.walking_right);
-		this.bitmapLeft = BitmapFactory.decodeResource(resources, R.drawable.walking_left);
+	private Rect sourceRect; // the rectangle to be drawn from the animation
+								// bitmap
+	private int frameNr = 5; // number of frames in animation
+
+	private int spriteWidth; // the width of the sprite to calculate the cut out
+								// rectangle
+	private int spriteHeight; // the height of the sprite
+
+	public Wall(Resources resources, int x, int y) {
+		this.bitmap = BitmapFactory.decodeResource(resources,
+				R.drawable.walking_right);
 		this.x = x;
 		this.y = y;
-		currentFrame = 0;
-		spriteWidth = bitmapRight.getWidth() / frameNr;
-		spriteHeight = bitmapRight.getHeight();
+		spriteWidth = bitmap.getWidth() / frameNr;
+		spriteHeight = bitmap.getHeight();
 		sourceRect = new Rect(0, 0, spriteWidth, spriteHeight);
-		frameTicker = 0l;
 	}
-	//for collision checks
-	public int getWidth(){
+
+	// for collision checks
+	public int getWidth() {
 		return spriteWidth;
 	}
-	//for collision checks
-	public int getHeight(){
+
+	// for collision checks
+	public int getHeight() {
 		return spriteHeight;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
 	}
 
-	/**
-	 * Method which updates the droid's internal state every tick
-	 */
 	public void update(long gameTime) {
 		// Walls don't need to update their state.
 	}
@@ -80,37 +72,10 @@ public class Wall {
 	// the draw method which draws the corresponding frame
 	public void draw(Canvas canvas) {
 		// where to draw the sprite
-		Rect destRect = new Rect(getX(), getY(), getX() + spriteWidth, getY() + spriteHeight);
+		Rect destRect = new Rect(getX(), getY(), getX() + spriteWidth, getY()
+				+ spriteHeight);
 		// pega no bitmap, corta pelo sourceRect e coloca em destRect
-		Bitmap bitmap = speed.getxDirection() == speed.DIRECTION_RIGHT ? bitmapRight : bitmapLeft;
-		canvas.drawBitmap(bitmap, sourceRect, destRect, null);
-//		canvas.drawBitmap(bitmap, 20, 150, null);
-//		Paint paint = new Paint();
-//		paint.setARGB(50, 0, 255, 0);
-//		canvas.drawRect(20 + (currentFrame * destRect.width()), 150, 20 + (currentFrame * destRect.width()) + destRect.width(), 150 + destRect.height(),  paint);
+		canvas.drawBitmap(this.bitmap, sourceRect, destRect, null);
 	}
-	
-//	public void draw(Canvas canvas) {
-//		canvas.drawBitmap(bitmap, x - (bitmap.getWidth() / 2), y - (bitmap.getHeight() / 2), null);
-//	}
-	
-	/**
-	 * Handles the {@link MotionEvent.ACTION_DOWN} event. If the event happens on the 
-	 * bitmap surface then the touched state is set to <code>true</code> otherwise to <code>false</code>
-	 * @param eventX - the event's X coordinate
-	 * @param eventY - the event's Y coordinate
-	 */
-//	public void handleActionDown(int eventX, int eventY) {
-//		if (eventX >= (x - bitmap.getWidth() / 2) && (eventX <= (x + bitmap.getWidth()/2))) {
-//			if (eventY >= (y - bitmap.getHeight() / 2) && (y <= (y + bitmap.getHeight() / 2))) {
-//				// droid touched
-//				setTouched(true);
-//			} else {
-//				setTouched(false);
-//			}
-//		} else {
-//			setTouched(false);
-//		}
-//
-//	}
+
 }
